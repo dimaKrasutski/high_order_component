5d2ddd042f37f70a9c41335e1219de58a37c258d
Dmitri Krasutski <krasutski.dmitri@gmail.com>
2018-10-17 21:42:34 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+
+
+
+
+export default ()=>{
+    return(
+        <div>
+            <CommentBox/>
+            <CommentList/>
+        </div>
+    )
+}
---
+++ src/reducers/comments.js
@@ -0,0 +1,16 @@
+import  {SAVE_COMMENT,FETCH_COMMENTS} from 'actions/types';
+
+export default function(state = [],action){ // reducer
+    switch(action.type){
+
+        case SAVE_COMMENT:
+                 return  [...state, action.payload]
+                 default:
+                 return state;
+         
+
+         case FETCH_COMMENTS:
+        const comments =  action.payload.data.map(comment => comment.name)
+            return  [...state,...comments]
+    }
+}
